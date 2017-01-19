e6f871d4eb36a731f7b8fb1679e1aaa18717b9ad
manojpolisetti22 <manojpolisetti22@gmail.com>
2017-01-19 18:51:57 -0500

    This is the Hello World Commit :D

---
+++ Solution.java
@@ -0,0 +1,10 @@
+package HeartRateProgram;
+
+/**
+ * Created by manojpolisetti on 19/01/17.
+ */
+public class Solution {
+    public static void main(String[] args){
+        System.out.println("Hello, World");
+    }
+}
