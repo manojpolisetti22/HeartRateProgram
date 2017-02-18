4f4ec63fbba18c47269fa05d4013e49eba90e067
Ruhana <razam@purdue.edu>
2017-02-18 14:54:17 -0500

    n/a

--- Trail.java
+++ Trail.java
@@ -1,8 +1,19 @@
 package HeartRateProgram;
+import java.util.Date;
 
 /**
  * Created by ruhana on 2/16/17.
  */
+
 public class Trail {
-    int age = 0;
+    private String trailID;
+    private Child childID;
+    private Date trailDate;
+
+    Trail (String trailID, Child childID, Date trailDate) {
+        this.trailID = trailID; // ID to THIS trail
+        this.childID = childID; // ID to the specific child
+        this.trailDate = trailDate;  // when the trail was taken
+    }
+
 }
