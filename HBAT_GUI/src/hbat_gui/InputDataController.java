3eb4e93663632a0248c2c86cac928e515f017023
Rajith Weerasinghe <rweerasi@purdue.edu>
2017-02-27 12:23:53 -0500

    Initial GUI Commit

---
+++ HBAT_GUI/src/hbat_gui/InputDataController.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hbat_gui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.stage.FileChooser;
+
+/**
+ * FXML Controller class
+ *
+ * @author Rajith
+ */
+public class InputDataController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+        FileChooser fileChooser = new FileChooser();
+        
+        
+    }    
+    
+    @FXML
+    public void getFileName( ) {
+        // TODO
+    }
+    
+}
