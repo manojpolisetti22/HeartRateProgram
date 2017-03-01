fbbf710006e48e9735ecdf65bd896bf8566375a7
Ruhana <razam@purdue.edu>
2017-03-01 12:43:25 -0500

    addition of algorithm class. calculates baseline. but is not tested yet.

--- Libraries/Algorithm.java
+++ Libraries/Algorithm.java
@@ -17,43 +17,55 @@ public class Algorithm {
 
 
     public static void main() {
-        LinkedList<Integer> timestamps = new LinkedList();
+        LinkedList<Double> timestamps = new LinkedList();
         Trial trail = new Trial("1000" ,"ChildA", new Date(2017,2,28), new Date(2017,2,28), Sex.FEMALE);
         Algorithm s = new Algorithm();
         s.calculateBase(timestamps, trail.getAttrubuteTable() );
 
     }
 
-    public int calculateBase( LinkedList<Integer> timeList, HashMap attributeTable) {
-        int [] lastFive = new int [5];
+    public int calculateBase( LinkedList<Double> timeList, HashMap<Double, Attribute> attributeTable) {
+        int[] lastFive = clearLastFive(new int[5]); // last five ibi's
         int baseLine = -1; // baseline since last look
         int looking = 0; // 0 = not looking; 1 == looking
+        int prevBaseLine = -1; // stores the previous baseline // this baseline is used for quick look aways
 
         for (int i = 0; i < timeList.size(); i++) {
-            int time = timeList.get(i);
+            double time = timeList.get(i);
 
             if (attributeTable.containsKey(time) && attributeTable.get(time) != null) { // CHECKS TO SEE THE KEY EXISTS
                 Attribute attribute = attributeTable.get(time);
-                Attribute att = attributeTable.get()
+                BehaviorAttribute bH = attribute.getbH();
 
-                if ( attribute.gethR() != null) { // add to the queue of last five
+
+                if (attribute.gethR().getRr() != -1) { // add's RR to lastFive if it exists
                     lastFive = addNewElement(lastFive, attribute.gethR().getRr());
                 }
-                if (attribute.getbH() != null) { // checks if behavioral attribute exists // THESE NEED TO BE FIXED
-                    BehaviorAttribute bH = attribute.getbH();
 
-                    if (bH.getCode_type() == CODE_TYPE.LOOK && bH.getEvent_type() == EVENT_TYPE.START) {
+
+                if (bH.getCode_type() == CODE_TYPE.LOOK && bH.getEvent_type() == EVENT_TYPE.START) { // start look
+                    if (getMedian(lastFive) == -1) {
+                        baseLine = prevBaseLine;
+                        prevBaseLine = -1;
+                    } else {
                         baseLine = getMedian(lastFive);
-                        looking = 1;
-                    } else if (bH.getCode_type() == CODE_TYPE.LOOK && bH.getEvent_type() == EVENT_TYPE.STOP) {
-                        looking = 0;
                     }
+                    looking = 1;
+                } else if (bH.getCode_type() == CODE_TYPE.LOOK && bH.getEvent_type() == EVENT_TYPE.STOP) { // stop look
+                    // resets everything
+                    looking = 0;
+                    lastFive = clearLastFive(lastFive);
+                    prevBaseLine = baseLine;
+                    baseLine = -1;
                 }
+
 
                 if (looking == 1) { // sets baseline whenever looking
                     attribute.getbH().setBaseLine(baseLine);
                 }
+
             }
+
         }
         return 0;
     }
@@ -67,13 +79,25 @@ public class Algorithm {
                 newArr[i + 1] = lastFive[i];
             }
         }
-        newArr[0] = newElement;
+         newArr[0] = newElement;
          return newArr;
     }
 
     int getMedian (int [] lastFive) {
+        for (int i = 0; i < lastFive.length; i++) { // checks that the look was long enough
+            if (lastFive[i] == -1) {return  -1;}
+        }
+
         Arrays.sort(lastFive);
         return lastFive[3];
     }
+
+    int [] clearLastFive (int [] lastFive) {
+        for (int i = 0; i < lastFive.length; i++) {
+            lastFive[i] = -1;
+        }
+        return lastFive;
+    }
+
 
 }
