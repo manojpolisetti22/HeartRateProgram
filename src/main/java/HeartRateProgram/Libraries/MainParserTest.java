e4d699feb3275a94d5704fc1f8949ea42d6f4f7f
Manoj Polisetti <manojpolisetti@pal-nat186-81-78.itap.purdue.edu>
2017-04-04 20:36:08 -0400

    New MainParser and MainPArserTest

---
+++ src/main/java/HeartRateProgram/Libraries/MainParserTest.java
@@ -0,0 +1,22 @@
+package HeartRateProgram.Libraries;
+
+/**
+ * Created by manojpolisetti on 3/28/17.
+ */
+
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MainParserTest {
+
+    private String rFileName;
+    private String bFileName;
+
+
+    @Test
+    public void testBehavioralParser(){
+        //Blah blah
+    }
+
+}
