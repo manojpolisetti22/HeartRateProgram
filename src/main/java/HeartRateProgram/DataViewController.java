424fc9e1950b246044a2628344d0a019fda4e832
Rajith Weerasinghe <rweerasi@purdue.edu>
2017-04-06 19:29:54 -0400

    Minor Changes

--- src/main/java/HeartRateProgram/DataViewController.java
+++ src/main/java/HeartRateProgram/DataViewController.java
@@ -29,66 +29,53 @@ import javafx.scene.control.cell.PropertyValueFactory;
  * @author Rajith
  */
 public class DataViewController implements Initializable {
+
     Stage thisStage;
     /**
      * Initializes the controller class.
      */
-    String mode; 
-    HashMap<Double,Attribute> data;
-    @FXML TableView table;
-    
+    String mode;
+    HashMap<Double, Attribute> data;
+    @FXML
+    TableView table;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO        
-        /*
-            Test data 
-        */
-        
-        /*ArrayList<Book> books = new ArrayList<>();
-        ObservableList<Book> b = FXCollections.observableArrayList(books);
 
-        books.add(new Book("Brave New World", "Aldous Huxely"));
-        books.add(new Book("To Kill a Mockingbird","Harper Lee"));
-        books.add(new Book("East of Eden","John Steinbeck"));
-        
-        TableColumn author = new TableColumn("Author");
-        table.getColumns().add(author); */
-        
-        
-        
-    }   
-    
-    public void initFiles(String participant_id, String rr_file, String behavior_file, 
+    }
+
+    public void initFiles(String participant_id, String rr_file, String behavior_file,
             double rr_start, double rr_sync, double behav_sync) {
         mode = "Basic";
-        
+
         // Parse Files
         MainParser parser = new MainParser();
         List<Double> rrList = parser.csvParserHeartRate(rr_file);
         List<Attribute> attrList = parser.csvParserBehavioral(behavior_file);
-        HashMap<Double,Attribute> parsedData = parser.finalParser(rrList, 
+        HashMap<Double, Attribute> parsedData = parser.finalParser(rrList,
                 attrList, rr_start, rr_sync, behav_sync);
-        
+
         // Analyze Dataset
         Algorithm algo = new Algorithm();
-        HashMap<Double,Attribute> processedData = algo.calculate(parsedData);
+        HashMap<Double, Attribute> processedData = algo.calculate(parsedData);
         processedData = algo.calculatePhases(processedData);
         //algo.printTable(processedData); 
-        
+
         // Get contents of table
         data = processedData;
         List<Double> timeList = algo.sortKeys(processedData);
         Collections.sort(timeList);
         //List<Attribute> contents = new ArrayList();
         final ObservableList<Attribute> contents = FXCollections.observableArrayList();
-        for(int i = 0; i < timeList.size(); i++) {
+        for (int i = 0; i < timeList.size(); i++) {
             Attribute attribute = processedData.get(timeList.get(i));
             contents.add(attribute);
         }
-        
+
         // Initialize table
         table.setEditable(false);
-        
+
         // Initialize columns with data
         TableColumn timeStampCol = new TableColumn("Timestamp");
         timeStampCol.setCellValueFactory(new PropertyValueFactory<>("timestamp"));
@@ -97,7 +84,7 @@ public class DataViewController implements Initializable {
         TableColumn eventTypeCol = new TableColumn("Event Type");
         eventTypeCol.setCellValueFactory(new PropertyValueFactory<>("Event_type"));
         TableColumn rRChangeCol = new TableColumn("RR Change");
-        rRChangeCol.setCellValueFactory(new PropertyValueFactory<>("rrChange")); 
+        rRChangeCol.setCellValueFactory(new PropertyValueFactory<>("rrChange"));
         TableColumn phaseCol = new TableColumn("Phase");
         phaseCol.setCellValueFactory(new PropertyValueFactory<>("phase"));
         TableColumn codeTypeCol = new TableColumn("Code Type");
@@ -106,30 +93,34 @@ public class DataViewController implements Initializable {
         eventNumCol.setCellValueFactory(new PropertyValueFactory<>("event_num"));
         TableColumn baselineCol = new TableColumn("Code Type");
         baselineCol.setCellValueFactory(new PropertyValueFactory<>("baseLine"));
-                
+
         // Add information to table
         table.setItems(contents);
         table.getColumns().addAll(timeStampCol, rrCol, phaseCol, eventTypeCol,
                 rRChangeCol, codeTypeCol, eventNumCol, baselineCol);
-        
+
     }
-    
+
     public void initFilesAdvanced(String file) {
         mode = "Advanced";
-        
-        // Add junk data
-        
+
+        // Do things
     }
-    
+
     public void export() {
         // Getting filename
-        FileChooser fileChooser = new FileChooser();
-        fileChooser.setTitle("Save Data");
-        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Comma Delimited File(*.csv)", "*.csv"));
-        File file = fileChooser.showSaveDialog(null);
-        
+        File file;
+        try {
+            FileChooser fileChooser = new FileChooser();
+            fileChooser.setTitle("Save Data");
+            fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Comma Delimited File(*.csv)", "*.csv"));
+            file = fileChooser.showSaveDialog(null);
+
+        } catch (Exception e) {
+            return; 
+        }
         String path = file.getAbsolutePath();
-        ConvertToCSV.convertToCSV(path,this.data);
+        ConvertToCSV.convertToCSV(path, this.data);
     }
 
 }
