fee8750937b9a78855b33f0cff5bc60132e3b961
Rajith Weerasinghe <rweerasi@purdue.edu>
2017-04-18 21:30:37 -0400

    Formatting

--- src/main/java/HeartRateProgram/Libraries/ConvertToCSV.java
+++ src/main/java/HeartRateProgram/Libraries/ConvertToCSV.java
@@ -1,5 +1,6 @@
 //package HeartRateProgram.HBAT.src.HeartRateProgram.Libraries;
 package HeartRateProgram.Libraries;
+
 import java.io.*;
 import java.util.Date;
 import java.util.HashMap;
@@ -10,39 +11,38 @@ import java.util.List;
  * Created by @ruhana on 3/2/17.
  */
 public class ConvertToCSV {
+
     private static final String COMMA_DELIMITER = ",";
     private static final String NEW_LINE = "\n";
     private static final String FILE_HEADER = "Timestamp, rr, CodeType, EventType, Event_Num, Base, rrChange, Phase ";
     private static final String UNKNOWN = ".";
 
-    public static void convertToCSV (String fileName, HashMap<Double, Attribute> table) {
-    Algorithm algo = new Algorithm();
-    List<Double> timestamps = algo.sortKeys(table);   
-     FileWriter fw;
-     BufferedWriter bw;
-        
+    public static void convertToCSV(String fileName, HashMap<Double, Attribute> table) {
+        Algorithm algo = new Algorithm();
+        List<Double> timestamps = algo.sortKeys(table);
+        FileWriter fw;
+        BufferedWriter bw;
+
         try { // MAYBE USE HANDLER INSTEAD!
             fw = new FileWriter(fileName);
             bw = new BufferedWriter(fw);
             bw.write(FILE_HEADER);
             bw.write(NEW_LINE);
-            
-            
-            for(int i = 0; i < timestamps.size(); i++) {
+
+            for (int i = 0; i < timestamps.size(); i++) {
                 double time = timestamps.get(i);
                 if (table.containsKey(time) && table.get(time) != null) { // checks for error
                     Attribute attribute = table.get(time);
                     BehaviorAttribute bH = attribute.getbH();
                     HeartBeatAttribute hR = attribute.gethR();
-                    
+
                     bw.write(Double.toString(time));
                     bw.write(COMMA_DELIMITER);
-                    
+
                     if (hR.getRr() == -1) {
-                        bw.write(UNKNOWN); 
-                    }
-                    else  {
-                        bw.write(Double.toString(hR.getRr())); 
+                        bw.write(UNKNOWN);
+                    } else {
+                        bw.write(Double.toString(hR.getRr()));
                     }
                     bw.write(COMMA_DELIMITER);
                     if (bH.getCode_type() == CODE_TYPE.NA) {
@@ -54,7 +54,7 @@ public class ConvertToCSV {
                     if (bH.getEvent_type() == EVENT_TYPE.NA) {
                         bw.write("");
                     } else {
-                        bw.write( bH.getEvent_type().toString() );
+                        bw.write(bH.getEvent_type().toString());
                     }
                     bw.write(COMMA_DELIMITER);
                     if (bH.getEvent_num() == -1) {
@@ -64,28 +64,28 @@ public class ConvertToCSV {
                     }
                     bw.write(COMMA_DELIMITER);
                     if (hR.getBaseLine() == -1) {
-                         bw.write(UNKNOWN);
+                        bw.write(UNKNOWN);
                     } else {
-                        bw.write(Double.toString(hR.getBaseLine()) );
+                        bw.write(Double.toString(hR.getBaseLine()));
                     }
                     bw.write(COMMA_DELIMITER);
                     if (hR.getBaseLine() == -1) { // checks to see if there is a behavioral data since hR
-                    // and bH data
-                    // cannot overlap
+                        // and bH data
+                        // cannot overlap
                         bw.write(UNKNOWN);
                     } else {
-                        bw.write(Double.toString(hR.getRrChange()) );
+                        bw.write(Double.toString(hR.getRrChange()));
                     }
                     bw.write(COMMA_DELIMITER);
-                    if(hR.getPhase() < 0 ) {
+                    if (hR.getPhase() < 0) {
                         bw.write(UNKNOWN);
                     } else {
                         bw.write(Integer.toString(hR.getPhase()));
                     }
-                    bw.write(NEW_LINE);  
-                } 
+                    bw.write(NEW_LINE);
+                }
             }
-            
+
             bw.close();
             fw.close();
 
@@ -93,24 +93,19 @@ public class ConvertToCSV {
             e.printStackTrace();
         }
     }
-    
-    
-    
-    
-    public static void main(String [] args) {
+
+    public static void main(String[] args) {
         LinkedList<Double> timestamps = new LinkedList();
-        Trial trail = new Trial("1000" ,"ChildA", new Date(2017,2,28), new Date(2017,2,28), Sex.FEMALE);
+        Trial trail = new Trial("1000", "ChildA", new Date(2017, 2, 28), new Date(2017, 2, 28), Sex.FEMALE);
         HashMap<Double, Attribute> table = new HashMap<>();
-        
-        
-        
+
         double currStamp = (double) 71680;
         timestamps.add(currStamp);
         table.put(currStamp, new Attribute(currStamp, 413));
         currStamp = (double) 72093;
         timestamps.add(currStamp);
         table.put(currStamp, new Attribute(currStamp, 413));
-        currStamp = (double) 72493 ;
+        currStamp = (double) 72493;
         timestamps.add(currStamp);
         table.put(currStamp, new Attribute(currStamp, 400));
         currStamp = (double) 72897;
@@ -137,7 +132,7 @@ public class ConvertToCSV {
         currStamp = (double) 75093;
         timestamps.add(currStamp);
         table.put(currStamp, new Attribute(currStamp, 413));
-        currStamp = (double) 75493 ;
+        currStamp = (double) 75493;
         timestamps.add(currStamp);
         table.put(currStamp, new Attribute(currStamp, 400));
         currStamp = (double) 75897;
@@ -149,28 +144,28 @@ public class ConvertToCSV {
         currStamp = (double) 75500;
         timestamps.add(currStamp);
         table.put(currStamp, new Attribute(currStamp, EVENT_TYPE.STOP, CODE_TYPE.LOOK));
- 
+
         trail.setAttributeTable(table);
 
-        convertToCSV("/Users/ruhana/testCSV" , table);
+        convertToCSV("/Users/ruhana/testCSV", table);
 
     }
 
-    public static int compare (String f1, String f2) {
+    public static int compare(String f1, String f2) {
         try {
             String r1 = readFileAsString(f1);
             String r2 = readFileAsString(f2);
 
-            r1.replace("\n" , ",");
-            r2.replace("\n" , ",");
+            r1.replace("\n", ",");
+            r2.replace("\n", ",");
 
-            String [] arr1 = r1.split(",");
-            String [] arr2 = r2.split(",");
+            String[] arr1 = r1.split(",");
+            String[] arr2 = r2.split(",");
 
-            if(arr1.length != arr2.length) {
+            if (arr1.length != arr2.length) {
                 return 0;
             }
-            for(int i = 0; i < arr1.length; i++) {
+            for (int i = 0; i < arr1.length; i++) {
                 if (!arr1[i].equals(arr2[i])) {
                     return 0;
                 }
@@ -187,8 +182,8 @@ public class ConvertToCSV {
         BufferedReader reader = new BufferedReader(
                 new FileReader(filePath));
         char[] buf = new char[1024];
-        int numRead=0;
-        while((numRead=reader.read(buf)) != -1){
+        int numRead = 0;
+        while ((numRead = reader.read(buf)) != -1) {
             String readData = String.valueOf(buf, 0, numRead);
             fileData.append(readData);
         }
